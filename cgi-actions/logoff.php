cbff7cf069f333d63aa8e5f3fea0b1a85acf1db5
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2019-12-18 17:03:48 -0300

    Done Logoff page [bugs not visualised ]

--- cgi-actions/logoff.php
+++ cgi-actions/logoff.php
@@ -1,5 +1,15 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/lpgp-server/template-master/raelgc/Template.php";
+use templateSystem\ErrorTemplate;
 session_start();
 
+if($_SESSION['user_logged'] != "true" || !isset($_SESSION['user_logged'])){
+    $err = new ErrorTemplate($_SERVER['DOCUMENT_ROOT'] . "/lpgp-server/core/templates/500-internal-error.html", "There's no user logged!", __FILE__, 7, "<button class=\"default-btn btn darkble-btn\" onclick=\"window.location.replace('http://localhost/lpgp-server/')\">Back to the index</button>");
+    die($err->parseFile());
+}
+else{
+    session_unset();
+    session_destroy();
+    echo "<script>window.location.replace('http://localhost/lpgp-server/');</script>";
+}
 ?>
