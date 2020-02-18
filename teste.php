47b4fbcbcd5e0bc7861cecf3a1b0b1b14dca6afe
GiullianoRossi1987 <giulliano.scatalon.rossi@gmail.com>
2020-02-18 12:28:19 -0300

    Servers management core done!

--- teste.php
+++ teste.php
@@ -4,6 +4,7 @@ require_once "./devcenter/devcore/dbapi.php";
 use Database\ServersManager;
 
 $t = new ServersManager("giulliano_php", "");
-echo $t->ckIP("198.178.0.0") ? "foi" : "nao foi";
+
+$t->sendSMPTTK("server1");
 
 ?>
